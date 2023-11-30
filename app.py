a503daebf17d6c96b2c7f42d3593141033280ff9
dcoxnard <dcoxnard89@gmail.com>
2023-11-30 05:02:34 -0800

    * hello world flask app * env yaml :

---
+++ app.py
@@ -0,0 +1,16 @@
+# Import the Flask class
+from flask import Flask
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+
+# Define a route and the associated function
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
