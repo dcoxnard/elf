394f99b7d041feb1b71f41b5f340636633a6873c
dcoxnard <dcoxnard89@gmail.com>
2023-12-05 11:48:39 -0800

    * display wishes

--- forms.py
+++ forms.py
@@ -4,7 +4,13 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
+
+
+class WishesForm(FlaskForm):
+    wish1 = StringField("Wish #1", validators=[DataRequired])
+    wish2 = StringField("Wish #2", validators=[DataRequired])
+    wish3 = StringField("Wish #3", validators=[DataRequired])
